8286ac6fe23d0413e74e17ab2cca03c88026e999
Rishav Tandon <rishavtandon@Rishavs-Air.home>
2023-10-10 16:57:40 +0200

    garbage 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -97,3 +97,51 @@ import { catchError, first, map, mergeMap, switchMap, tap } from 'rxjs/operators
 //     })
 //   );
 // }
+
+// import { zip } from 'rxjs';
+// import { catchError, first, map, mergeMap, switchMap, tap } from 'rxjs/operators';
+
+// private onDealableMultiple() {
+//   return this.events.onDealableMultiplePricers().pipe(
+//     mergeMap((payload: { pricerIds: string[] }) => {
+//       const pricers$ = this.pricerRepo.getPricerbyId(payload.pricerIds);
+
+//       return pricers$.pipe(
+//         first(),
+//         switchMap((pricers: Pricer[]) => {
+//           const isMifidObservables = pricers.map((pricer) =>
+//             this.IsMifidRequired(pricer.pricerId).pipe(
+//               catchError(() => of(undefined)) // Handle errors by setting to undefined
+//             )
+//           );
+
+//           return zip(...isMifidObservables, (...results) => {
+//             const resultObject = {};
+//             payload.pricerIds.forEach((pricerId, index) => {
+//               resultObject[pricerId] = results[index];
+//             });
+//             return resultObject;
+//           }).pipe(
+//             tap((results) => {
+//               // Log the results
+//               console.log('IsMifidRequired results:', results);
+//             }),
+//             map((results) => ({
+//               pricers,
+//               mifidResults: results,
+//             }))
+//           );
+//         }),
+//         mergeMap(({ pricers, mifidResults }) => {
+//           // Now, you can use pricers and mifidResults as needed
+//           return this.dealableSpecificPricers(pricers, mifidResults);
+//         })
+//       );
+//     })
+//   );
+// }
+
+
+
+
+
