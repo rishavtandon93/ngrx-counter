56a68a2b41e3d4f4c4e3bb5589d57d5c489db9bf
Rishav Tandon <rishavtandon@112.sub-97-13-227.myvzw.com>
2023-10-11 14:11:34 +0200

    garbage 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -174,48 +174,74 @@ import { catchError, first, map, mergeMap, switchMap, tap } from 'rxjs/operators
 // }
 
 
-private onDealableMultiple() {
-  return this.events.onDealableMultiplePricers().pipe(
-    mergeMap((payload: { pricerIds: string[], clientRequestTime: string }) => {
-      const pricers$ = this.pricerRepo.getPricerbyId(payload.pricerIds);
+// private onDealableMultiple() {
+//   return this.events.onDealableMultiplePricers().pipe(
+//     mergeMap((payload: { pricerIds: string[], clientRequestTime: string }) => {
+//       const pricers$ = this.pricerRepo.getPricerbyId(payload.pricerIds);
 
-      return pricers$.pipe(
-        first(),
-        switchMap((pricers: Pricer[]) => {
-          const isMifidObservables = pricers.map((pricer) =>
-            this.IsMifidRequired(pricer.pricerId).pipe(
-              catchError(() => of(undefined)) // Handle errors by setting to undefined
-            )
-          );
+//       return pricers$.pipe(
+//         first(),
+//         switchMap((pricers: Pricer[]) => {
+//           const isMifidObservables = pricers.map((pricer) =>
+//             this.IsMifidRequired(pricer.pricerId).pipe(
+//               catchError(() => of(undefined)) // Handle errors by setting to undefined
+//             )
+//           );
 
-          return zip(...isMifidObservables, (...results) => {
-            const resultObjects = payload.pricerIds.map((pricerId, index) => {
-              const isMIfidRequired = results[index];
-              const resultObject = { id: pricerId, isMIfidRequired };
+//           return zip(...isMifidObservables, (...results) => {
+//             const resultObjects = payload.pricerIds.map((pricerId, index) => {
+//               const isMIfidRequired = results[index];
+//               const resultObject = { id: pricerId, isMIfidRequired };
 
-              if (isMIfidRequired) {
-                resultObject.clientReqeustTime = payload.clientRequestTime;
-              }
+//               if (isMIfidRequired) {
+//                 resultObject.clientReqeustTime = payload.clientRequestTime;
+//               }
 
-              return resultObject;
-            });
+//               return resultObject;
+//             });
 
-            return resultObjects;
-          }).pipe(
-            tap((results) => {
-              // Log the results
-              console.log('IsMifidRequired results:', results);
-            }),
-            mergeMap((results) => {
-              // Now, you can use pricers and results as needed
-              return this.dealableSpecificPricers(pricers, results);
-            })
-          );
-        })
-      );
-    })
-  );
-}
+//             return resultObjects;
+//           }).pipe(
+//             tap((results) => {
+//               // Log the results
+//               console.log('IsMifidRequired results:', results);
+//             }),
+//             mergeMap((results) => {
+//               // Now, you can use pricers and results as needed
+//               return this.dealableSpecificPricers(pricers, results);
+//             })
+//           );
+//         })
+//       );
+//     })
+//   );
+// }
+
+
+// private dealableSpecificPricers(pricers: Pricer[], resultObjects: ResultObject[]) {
+//   return merge(
+//     pricers.map((pricer, index) => {
+//       const resultObject = resultObjects[index];
+//       const isMet = resultObject.id && resultObject.isMIfidRequired === true && resultObject.clientReqeustTime !== null && resultObject.clientReqeustTime !== undefined;
+
+//       return of(isMet);
+//     })
+//   ).pipe(
+//     mergeAll(),
+//     mergeMap((isValid) => {
+//       console.log('isValid', isValid);
+
+//       if (isValid) {
+//         return PricerActions.calculatePriceRequested({
+//           workspaceID: pricers.id,
+//           formID: PricerConfigEnum.FormId
+//         });
+//       } else {
+//         return of(); // You can adjust this part based on your use case
+//       }
+//     })
+//   );
+// }
 
 
 
