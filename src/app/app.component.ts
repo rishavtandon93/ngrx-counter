5a508140eb23a235139b14997d7273844c312569
Rishav Tandon <rishavtandon@112.sub-97-13-227.myvzw.com>
2024-10-02 12:03:54 +0200

    new-22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,3 +64,38 @@ import { NumberCommaDirective } from './directives/number-comma.directive';
   bootstrap: [MyFormComponent]
 })
 export class AppModule { }
+
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { NgControl } from '@angular/forms';
+
+@Directive({
+  selector: '[appNumberComma]',
+})
+export class NumberCommaDirective {
+  constructor(private el: ElementRef, private control: NgControl) {}
+
+  @HostListener('input', ['$event'])
+  onInputChange(event: any): void {
+    // Get the value from the input and remove all commas
+    let inputValue = this.el.nativeElement.value.replace(/,/g, '');
+
+    if (!isNaN(Number(inputValue)) && inputValue !== '') {
+      // Format the number with commas
+      this.el.nativeElement.value = this.formatNumberWithCommas(inputValue);
+    }
+
+    // Update the form control with the unformatted number
+    this.control.control?.setValue(inputValue);
+  }
+
+  @HostListener('blur', ['$event'])
+  onBlur(event: any): void {
+    // Ensure correct formatting with commas on blur
+    this.el.nativeElement.value = this.formatNumberWithCommas(this.el.nativeElement.value);
+  }
+
+  private formatNumberWithCommas(value: string): string {
+    return value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+}
+
