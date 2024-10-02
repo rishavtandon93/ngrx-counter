5a0449a7a3faf48ed5fe768df5acd3c0a3c72caa
Rishav Tandon <rishavtandon@112.sub-97-13-227.myvzw.com>
2024-10-02 13:04:27 +0200

    new-23

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,3 @@
-<form [formGroup]="myForm" (ngSubmit)="onSubmit()">
-  <div>
-    <label for="name">Name:</label>
-    <input type="text" id="name" formControlName="name" />
-  </div>
-
-  <div>
-    <label for="amount">Amount:</label>
-    <input
-      type="text"
-      id="amount"
-      formControlName="amount"
-      appNumberComma
-    />
-  </div>
-
-  <button type="submit">Submit</button>
-</form>
-
-
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-@Component({
-  selector: 'app-my-form',
-  templateUrl: './my-form.component.html',
-})
-export class MyFormComponent {
-  myForm: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.myForm = this.fb.group({
-      name: ['', Validators.required],
-      amount: ['', Validators.required],
-    });
-  }
-
-  onSubmit(): void {
-    if (this.myForm.valid) {
-      const formValues = this.myForm.value;
-      console.log('Form submitted:', formValues);
-      // You can now use formValues.amount as a pure number (without commas)
-    }
-  }
-}
-
-
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { MyFormComponent } from './my-form/my-form.component';
-import { NumberCommaDirective } from './directives/number-comma.directive';
-
-@NgModule({
-  declarations: [
-    MyFormComponent,
-    NumberCommaDirective // Add the directive here
-  ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-  ],
-  providers: [],
-  bootstrap: [MyFormComponent]
-})
-export class AppModule { }
-
 import { Directive, ElementRef, HostListener } from '@angular/core';
 import { NgControl } from '@angular/forms';
 
@@ -72,30 +5,57 @@ import { NgControl } from '@angular/forms';
   selector: '[appNumberComma]',
 })
 export class NumberCommaDirective {
+  private lastCursorPosition: number = 0;
+
   constructor(private el: ElementRef, private control: NgControl) {}
 
   @HostListener('input', ['$event'])
   onInputChange(event: any): void {
-    // Get the value from the input and remove all commas
-    let inputValue = this.el.nativeElement.value.replace(/,/g, '');
+    const input = this.el.nativeElement;
 
-    if (!isNaN(Number(inputValue)) && inputValue !== '') {
+    // Get the position of the cursor before formatting
+    const cursorPosition = input.selectionStart;
+    const originalValue = input.value;
+
+    // Remove commas
+    let numericValue = originalValue.replace(/,/g, '');
+
+    // Set the new raw numeric value to the form control
+    this.control.control?.setValue(numericValue);
+
+    if (!isNaN(Number(numericValue)) && numericValue !== '') {
       // Format the number with commas
-      this.el.nativeElement.value = this.formatNumberWithCommas(inputValue);
+      input.value = this.formatNumberWithCommas(numericValue);
     }
 
-    // Update the form control with the unformatted number
-    this.control.control?.setValue(inputValue);
+    // Adjust the cursor position after reformatting
+    this.adjustCursorPosition(cursorPosition, originalValue, input);
   }
 
   @HostListener('blur', ['$event'])
   onBlur(event: any): void {
-    // Ensure correct formatting with commas on blur
-    this.el.nativeElement.value = this.formatNumberWithCommas(this.el.nativeElement.value);
+    // Ensure the input is formatted properly when the input field loses focus
+    const input = this.el.nativeElement;
+    input.value = this.formatNumberWithCommas(input.value.replace(/,/g, ''));
   }
 
   private formatNumberWithCommas(value: string): string {
     return value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   }
-}
 
+  // Function to adjust the cursor position after formatting
+  private adjustCursorPosition(
+    cursorPosition: number,
+    originalValue: string,
+    input: HTMLInputElement
+  ): void {
+    const newFormattedValue = input.value;
+    const lengthDiff = newFormattedValue.length - originalValue.length;
+
+    // Set the new cursor position, adjusting for the difference caused by adding commas
+    input.setSelectionRange(
+      cursorPosition + lengthDiff,
+      cursorPosition + lengthDiff
+    );
+  }
+}
