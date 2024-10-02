3c0fef8b772c94bd0d9035740bfce8e89c7871d2
Rishav Tandon <rishavtandon@112.sub-97-13-227.myvzw.com>
2024-10-02 12:02:26 +0200

    new-21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,66 @@
-processIds() {
-  const inputString = this.idsControl.value;
+<form [formGroup]="myForm" (ngSubmit)="onSubmit()">
+  <div>
+    <label for="name">Name:</label>
+    <input type="text" id="name" formControlName="name" />
+  </div>
 
-  // Split input string based on commas, spaces, or newlines
-  const idArray = inputString
-    .split(/[\s,]+/) // Regular expression to split by spaces, commas, or newlines
-    .filter(id => id.trim() !== ''); // Filter out empty strings
+  <div>
+    <label for="amount">Amount:</label>
+    <input
+      type="text"
+      id="amount"
+      formControlName="amount"
+      appNumberComma
+    />
+  </div>
 
-  console.log(idArray);
-  // You can now use this array for further processing
+  <button type="submit">Submit</button>
+</form>
+
+
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-my-form',
+  templateUrl: './my-form.component.html',
+})
+export class MyFormComponent {
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      amount: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.myForm.valid) {
+      const formValues = this.myForm.value;
+      console.log('Form submitted:', formValues);
+      // You can now use formValues.amount as a pure number (without commas)
+    }
+  }
 }
+
+
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { MyFormComponent } from './my-form/my-form.component';
+import { NumberCommaDirective } from './directives/number-comma.directive';
+
+@NgModule({
+  declarations: [
+    MyFormComponent,
+    NumberCommaDirective // Add the directive here
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [MyFormComponent]
+})
+export class AppModule { }
