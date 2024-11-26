0a2ac3e9a574e841bf72d42538bb38ff21e79c32
Rishav Tandon <rishavtandon@Rishavs-MacBook-Air.local>
2024-11-26 15:32:32 +0100

    new-51

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
-function findHighestValue(arr) {
-  return arr.reduce((max, obj) => {
-    return obj.value !== null && obj.value > max ? obj.value : max;
-  }, 0);
+function transformAttribution(): Observable<any> {
+  return calculateAttribution().pipe(
+    map((data) => {
+      if (data.Result && data.Result.Rows.length > 0) {
+        const firstRow = data.Result.Rows[0];
+        return Object.keys(firstRow).map((key) => ({
+          field: key,
+          headerName: key,
+        }));
+      }
+      return []; // Return an empty array if Result or Rows is not present
+    })
+  );
 }
