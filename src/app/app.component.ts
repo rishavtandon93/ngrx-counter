9320f7e6bf81cbbc6182e023ae39b649a62ee987
Rishav Tandon <rishavtandon@Rishavs-MacBook-Air.local>
2024-11-27 11:45:53 +0100

    new-53

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,6 @@
-const createTransaction = (key: keyof typeof AttributionResult) => {
-  const row = data?.[key]?.['Rows']?.[0];
-  if (row) {
-    return { ...row, type: key };
-  }
-  return null;
-};
+const order = ['type', 'Quote_Id', 'Product_Description'];
 
-const transactions = [
-  createTransaction(AttributionResult.Result),
-  createTransaction(AttributionResult.ResultRawPrice),
-].filter(Boolean); // Filter out null values
-
-transactions.forEach((transaction) => {
-  this.gridApi.applyTransaction({ add: [transaction] });
-});
+const reorderedColumns = [
+  ...columns.filter((col) => order.includes(col.field)), // Top-priority fields
+  ...columns.filter((col) => !order.includes(col.field)), // Remaining fields
+];
